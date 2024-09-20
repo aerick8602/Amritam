e8bd4492329dfdd7dc697a1defed7ad05dfad678
Ayush Katiyar <148551722+aerick8602@users.noreply.github.com>
2024-09-20 14:19:05 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,39 +5,37 @@ import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div className="h-screen bg-gradient-to-b from-green-600 to-green-950 flex flex-col justify-center items-center text-white text-center relative">
-      <Image
-        className="w-[350px] mb-8 hover:scale-125 transition-transform duration-1000 ease-in-out blur-md invert drop-shadow-xl md:filter-none"
-        src="https://www.amritam.co/amritam.png"
-        alt="Amritam Logo"
-        width={350}
-        height={200} // Adjust height as needed
-      />
-      <h1 className="flex gap-12 text-5xl md:text-7xl font-bold mb-4">
-        <p className="hover:scale-110 transition-transform duration-500 ease-in-out bg-gradient-to-r from-green-100 via-white to-green-500 text-transparent bg-clip-text">
-          Welcome
+    <div className="flex flex-col h-screen bg-gradient-to-b from-green-600 to-green-950 text-white">
+      <div className="flex-grow flex flex-col justify-center items-center text-center p-4">
+        <Image
+          className="w-[350px] mb-8 hover:scale-110 transition-transform duration-1000 ease-in-out blur-md invert drop-shadow-xl md:filter-none"
+          src="https://www.amritam.co/amritam.png"
+          alt="Amritam Logo"
+          width={350}
+          height={210}
+        />
+        <h1 className="flex flex-wrap justify-center gap-6 text-5xl md:text-7xl font-bold mb-4">
+          <span className="hover:scale-110 transition-transform duration-500 ease-in-out bg-gradient-to-r from-green-100 via-white to-green-500 text-transparent bg-clip-text">
+            Welcome
+          </span>
+          <span className="hover:scale-110 transition-transform duration-500 ease-in-out bg-gradient-to-r from-green-100 via-white to-green-500 text-transparent bg-clip-text">
+            to
+          </span>
+          <span className="hover:scale-110 transition-transform duration-500 ease-in-out bg-gradient-to-r from-green-100 via-white to-green-500 text-transparent bg-clip-text">
+            Amritam
+          </span>
+        </h1>
+        <p className="text-lg md:text-xl mb-8 mx-4 transition-all duration-300 ease-in-out">
+          Reintroducing natural and organic products to nourish your body and mind. Experience the vitality of pure, wholesome food.
         </p>
-        <p className="hover:scale-110 transition-transform duration-500 ease-in-out bg-gradient-to-r from-green-100 via-white to-green-500 text-transparent bg-clip-text">
-          to
-        </p>
-        <p className="hover:scale-110 transition-transform duration-500 ease-in-out bg-gradient-to-r from-green-100 via-white to-green-500 text-transparent bg-clip-text">
-          Amritam
-        </p>
-      </h1>
-      <p className="text-lg md:text-xl mb-8 mx-4 transition-all duration-300 ease-in-out">
-        Reintroducing natural and organic products to nourish your body and mind. Experience the vitality of pure, wholesome food.
-      </p>
-      <Link href="/components/signup">
-        <button className="bg-green-700 px-10 py-4 rounded-full font-bold text-xl cursor-pointer hover:bg-green-800 transition-transform transform hover:scale-105 shadow-lg">
-          Explore
-        </button>
-      </Link>
-
-
-      <footer className="fixed bottom-0 left-0 w-full py-4 shadow-lg bg-gradient-to-b from-green-600 to-green-950 text-gray-300 text-sm md:text-base">
-
-        <p className="absolute left-4">&copy; {new Date().getFullYear()} Amritam. All rights reserved.</p>
-        <p className="text-center">Developed by Ayush Katiyar</p>
+        <Link href="/components/signup">
+          <button className="bg-green-700 px-10 py-4 rounded-full font-bold text-xl cursor-pointer hover:bg-green-800 transition-transform transform hover:scale-110 transition-transform duration-300 ease-in-out shadow-lg">
+            Explore
+          </button>
+        </Link>
+      </div>
+      <footer className="text-gray-300 py-1 w-full text-center text-sm md:text-base opacity-75">
+        <p>&copy; {new Date().getFullYear()} Amritam. All rights reserved | Developed by Ayush Katiyar</p>
       </footer>
     </div>
   );
